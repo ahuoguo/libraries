2e749e039c89c9266a82f36bb785a15c35dfb01f
ahuoguo <ahuoguo@gmail.com>
2023-06-23 18:56:53 -0400

    proved all non-auto lemmas in Mul

--- src/NonLinearArith/Internals/MulInternalsNonlinear.rs
+++ src/NonLinearArith/Internals/MulInternalsNonlinear.rs
@@ -11,32 +11,32 @@ verus! {
 
 /* multiplying two positive integers will result in a positive integer */
 #[verifier(nonlinear)]
-proof fn lemma_mul_strictly_positive(x: int, y: int)
+pub proof fn lemma_mul_strictly_positive(x: int, y: int)
     ensures (0 < x && 0 < y) ==> (0 < x * y)
 {}
 
 /* Integral Domain */
 /* multiplying two nonzero integers will never result in 0 as the poduct */
 #[verifier(nonlinear)]
-proof fn lemma_mul_nonzero(x: int, y: int)
+pub proof fn lemma_mul_nonzero(x: int, y: int)
     ensures x * y != 0 <==> x != 0 && y != 0
 {}
 
 /* multiplication is associative */
 #[verifier(nonlinear)]
-proof fn lemma_mul_associative(x: int, y: int, z: int)
+pub proof fn lemma_mul_is_associative(x: int, y: int, z: int)
     ensures x * (y * z) == (x * y) * z
 {}
 
 /* multiplication is distributive */
 #[verifier(nonlinear)]
-proof fn lemma_mul_distributive_add(x: int, y: int, z: int)
+pub proof fn lemma_mul_is_distributive_add(x: int, y: int, z: int)
     ensures x * (y + z) == x * y + x * z
 {}
 
 /* the product of two integers is greater than the value of each individual integer */
 #[verifier(nonlinear)]
-proof fn lemma_mul_ordering(x: int, y: int)
+pub proof fn lemma_mul_ordering(x: int, y: int)
     requires 
         x != 0,
         y != 0,
@@ -47,7 +47,7 @@ proof fn lemma_mul_ordering(x: int, y: int)
 
 /* multiplying by a positive integer preserves inequality */
 #[verifier(nonlinear)]
-proof fn lemma_mul_strict_inequality(x: int, y: int, z: int)
+pub proof fn lemma_mul_strict_inequality(x: int, y: int, z: int)
     requires
         x < y,
         z > 0
